5094e6abe8cdf4b3dccdf883d4c2aadfee2b96e0
Tobias Gallo <tobiasgallo89@gmail.com>
2026-02-27 22:38:13 -0300

    cambio de base de datos

--- js/config/supabase.js
+++ js/config/supabase.js
@@ -3,8 +3,8 @@
    ======================================== */
 
 // TODO: Reemplazar con tus credenciales de Supabase
-const SUPABASE_URL = 'https://llasocseadxsndqfibls.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsYXNvY3NlYWR4c25kcWZpYmxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNTgxNDgsImV4cCI6MjA4NjgzNDE0OH0.V2Vqo0SGaKtyV1OWVfNuBx-7JbHTGd_rKhoAbgNy7sM';
+const SUPABASE_URL = 'https://zcxarirzmghjwezzxvoh.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjeGFyaXJ6bWdoandlenp4dm9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMzkwODAsImV4cCI6MjA4NzgxNTA4MH0.UWdl0MzuCaOwnvCHC1gsPuFchW3sOHsI5dHO1y5J--4';
 
 // Inicializar cliente de Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
